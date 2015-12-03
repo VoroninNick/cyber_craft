2a8cb65f5cc1754abdf7eac27c86c1f2224ba0bf
VoroninNick <p.korenev@voroninstudio.eu>
2015-12-03 12:20:59 +0200

    latest changes from html repository

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,30 +1,30 @@
 
 // js main heder superslides
 $(document).ready(function() {
-  $(document).on('init.slides', function() {
-    $('.loading-container').fadeOut(function() {
-      $(this).remove();
+    $(document).on('init.slides', function() {
+        $('.loading-container').fadeOut(function() {
+            $(this).remove();
+        });
     });
-  });
 
-  $('#slides').superslides({
-    slide_easing: 'easeInOutCubic',
-    slide_speed: 800,
-    pagination: true,
-    animation: 'fade',
-    hashchange: false,
-    play: 5000,
-    scrollable: true
-  });
+    $('#slides').superslides({
+        slide_easing: 'easeInOutCubic',
+        slide_speed: 800,
+        pagination: true,
+        animation: 'fade',
+        hashchange: false,
+        play: 5000,
+        scrollable: true
+    });
 
 
-  $('#slides').hammer().on('swipeleft', function() {
-    $(this).superslides('animate', 'next');
-  });
+    $('#slides').hammer().on('swipeleft', function() {
+        $(this).superslides('animate', 'next');
+    });
 
-  $('#slides').hammer().on('swiperight', function() {
-    $(this).superslides('animate', 'prev');
-  });
+    $('#slides').hammer().on('swiperight', function() {
+        $(this).superslides('animate', 'prev');
+    });
 
 });
 //And js main heder superslides
@@ -36,28 +36,28 @@ $(function() {
 
 
 // Sidebar Navigation  Main page
-    $(document).ready(function () {
+$(document).ready(function () {
 
 
-        $('.scroll-container').onePageScroll({
-            topElement: $('header'),
+    $('.scroll-container').onePageScroll({
+        topElement: $('header'),
 //            position: 'left',
 //            background: 'rgba(50,255,100, 0.9)',
 //            tooltip: false,
 //            tooltipSelector: $('h3'),
 //            emptyContent: true,
-            sectionSelector: $('.content-part')
-        });
-
-        $('.setOff').click(function () {
-            console.log('off');
-            $('.scroll-container').onePageScrollDestroy({
-              offsetAktiv: 0,
-              topMargin: 0
-
-            });
-        })
+        sectionSelector: $('.content-part')
     });
+
+    $('.setOff').click(function () {
+        console.log('off');
+        $('.scroll-container').onePageScrollDestroy({
+            offsetAktiv: 0,
+            topMargin: 0
+
+        });
+    })
+});
 //And Sidebar Navigation  Main page
 
 
@@ -81,68 +81,70 @@ $(function() {
 
 // Slider Main page
 $(document).ready(function() {
- 
-  $("#reviews").owlCarousel({
-    pagination: false,
-    navigation: true,
-    singleItem : true
-  });
- 
+
+    $("#reviews").owlCarousel({
+        pagination: false,
+        navigation: true,
+        singleItem : true
+    });
+
 });
 //End Slider Main page
 
 
 // Slider Main page
 $(document).ready(function(){
-  $('.industrieswehelp').bxSlider({
-    pagerSelector: '.industriewwepager'
-  });
+    $('.industrieswehelp').bxSlider({
+        pagerSelector: '.industriewwepager',
+        auto: true
+    });
 });
 //End Slider Main page
 // Animated css scroll
 $(document).ready(function(){
-new WOW().init();
+    new WOW().init();
 });
 // Animated css scroll
 // Form animated Main page
 $(document).ready(function() {
 
-  $('input').each(function() {
+    $('input').each(function() {
 
-    $(this).on('focus', function() {
-      $(this).parent('.css').addClass('active');
+        $(this).on('focus', function() {
+            $(this).parent('.css').addClass('active');
+        });
+
+        $(this).on('blur', function() {
+            if ($(this).val().length == 0) {
+                $(this).parent('.css').removeClass('active');
+            }
+        });
+
+        if ($(this).val() != '') $(this).parent('.css').addClass('active');
+
     });
-
-    $(this).on('blur', function() {
-      if ($(this).val().length == 0) {
-        $(this).parent('.css').removeClass('active');
-      }
-    });
-
-    if ($(this).val() != '') $(this).parent('.css').addClass('active');
-
-  });
 
 });
 
 
 $(document).ready(function() {
 
-  $('textarea').each(function() {
+    $('textarea').each(function() {
 
-    $(this).on('focus', function() {
-      $(this).parent('.css').addClass('active');
+        $(this).on('focus', function() {
+            $(this).parent('.css').addClass('active');
+        });
+
+        $(this).on('blur', function() {
+            if ($(this).val().length == 0) {
+                $(this).parent('.css').removeClass('active');
+            }
+        });
+
+        if ($(this).val() != '') $(this).parent('.css').addClass('active');
+
     });
-
-    $(this).on('blur', function() {
-      if ($(this).val().length == 0) {
-        $(this).parent('.css').removeClass('active');
-      }
-    });
-
-    if ($(this).val() != '') $(this).parent('.css').addClass('active');
-
-  });
 
 });
 //And Form animated Main page
+
