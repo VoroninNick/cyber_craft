03dc6244238d58d3c2bcad50d7df512e1679ec80
VoroninNick <p.korenev@voroninstudio.eu>
2015-12-04 18:23:24 +0200

    home articles

--- app/models/blog_article.rb
+++ app/models/blog_article.rb
@@ -1,9 +1,22 @@
 class BlogArticle < ActiveRecord::Base
-  acts_as_article(tags: true)
+  acts_as_article(tags: true, initialize_all_attachments: false)
+
+  has_attached_file :avatar, styles: { home: "720x450#" }
+
+  [:avatar].each do |attachment_name|
+
+    do_not_validate_attachment_file_type attachment_name
+    attr_accessible attachment_name
+  end
 
   scope :home_articles, -> { last(3) }
 
+  def author_name
+    "Tomas Elbow"
+  end
 
 
-
+  def article_date
+    updated_at
+  end
 end
