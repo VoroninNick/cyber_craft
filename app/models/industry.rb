f62b0deb74765f5268a4a91de6c980b8318dc889
VoroninNick <p.korenev@voroninstudio.eu>
2015-12-04 14:39:40 +0200

    image

--- app/models/industry.rb
+++ app/models/industry.rb
@@ -2,7 +2,7 @@ class Industry < ActiveRecord::Base
   acts_as_article(base_articles: BaseArticles)
 
   [:index_logo].each do |attachment_name|
-    has_attached_file attachment_name, styles: {home_logo: "1000x500#" }
+    has_attached_file attachment_name, styles: {home_logo: "940x470#" }
     do_not_validate_attachment_file_type attachment_name
     attr_accessible attachment_name
   end
