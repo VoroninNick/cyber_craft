0bffdbda2ecd4458999d32b230390599cef9dd2c
VoroninNick <p.korenev@voroninstudio.eu>
2015-12-04 14:49:03 +0200

    fix

--- app/models/industry.rb
+++ app/models/industry.rb
@@ -1,15 +1,15 @@
 class Industry < ActiveRecord::Base
-  acts_as_article(base_articles: BaseArticles)
+ # acts_as_article(base_articles: BaseArticles)
 
   [:index_logo].each do |attachment_name|
     has_attached_file attachment_name, styles: {home_logo: "940x470#" }
     do_not_validate_attachment_file_type attachment_name
     attr_accessible attachment_name
   end
-
-  def self.reprocess_images!
-    self.all.each do |item|
-
-    end
-  end
+  #
+  # def self.reprocess_images!
+  #   self.all.each do |item|
+  #
+  #   end
+  # end
 end
