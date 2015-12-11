8deac4dd0b67fb629ed1570a463c1fc4bc52010d
VoroninNick <p.korenev@voroninstudio.eu>
2015-12-11 14:58:12 +0200

    add fields to template

--- app/helpers/base_industries_helper.rb
+++ app/helpers/base_industries_helper.rb
@@ -1,10 +1,11 @@
 module BaseIndustriesHelper
-  def description_with_image(image, description, header = nil, svg = false, image_columns = 3, image_alignment = :left)
-    opts = { image: image, description: description, header: header, image_columns: image_columns, image_alignment: image_alignment, svg: svg }
+  def description_with_image(image, description, **options)
+    defaults = {svg: false, image_columns: 3, image_alignment: :left, image: image, description: description, header: nil}
+    opts = defaults.merge(options)
     render partial: "base_industries/description_with_image", locals: {opts: opts}
   end
 
-  def _full_width_banner_with_title(image, title)
+  def full_width_banner_with_title(image, title)
     render partial: "base_industries/full_width_banner_with_title", locals: { opts: { image: image, title: title } }
   end
 end
