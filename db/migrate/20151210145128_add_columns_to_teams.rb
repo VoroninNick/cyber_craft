689e5978f03820a166d767c65c3b40b5ab625103
VoroninNick <p.korenev@voroninstudio.eu>
2015-12-11 14:16:29 +0200

    update teams table

--- db/migrate/20151210145128_add_columns_to_teams.rb
+++ db/migrate/20151210145128_add_columns_to_teams.rb
@@ -10,9 +10,13 @@ class AddColumnsToTeams < ActiveRecord::Migration
       t.attachment :section_2_image
       t.text :section_2_image_description
 
-      t.string :section_3_background
-      t.text :section_3_description
-      t.attachment :section_3_image
+      t.string :section_3a_background
+      t.text :section_3a_description
+      t.attachment :section_3a_image
+
+      t.string :section_3b_background
+      t.text :section_3b_description
+      t.attachment :section_3b_image
 
       t.attachment :section_4_image
       t.text :section_4_image_description
