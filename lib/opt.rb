aa96426123f38cd336ff560b794be2e00d91c812
VoroninNick <p.korenev@voroninstudio.eu>
2016-02-04 17:45:24 +0200

    assign new path

--- lib/opt.rb
+++ lib/opt.rb
@@ -30,8 +30,9 @@ module Opt
     FileUtils.rm_rf(optimized_assets_path)
     FileUtils.copy_entry(original_assets_path, optimized_assets_path)
     #AssetImageOpt::WORKING_DIR = optimized_images_relative_path
+    AssetImageOpt::WORKING_DIR.send(:replace, optimized_images_relative_path)
     opt = AssetImageOpt.new
-    opt
+
     opt.optimize
   end
 end
