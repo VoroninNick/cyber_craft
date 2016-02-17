1c00785b3e543fa62a0990a2ec7ff2d989e7edd9
VoroninNick <p.korenev@voroninstudio.eu>
2016-02-17 11:56:08 +0200

    set paperclip path

--- config/initializers/paperclip_defaults.rb
+++ config/initializers/paperclip_defaults.rb
@@ -1,3 +1,9 @@
+# interpolate in paperclip
+Paperclip.interpolates :public_path  do |attachment, style|
+    Rails.public_path
+end
+
 Paperclip::Attachment.default_options.merge!(
-    processors: [:thumbnail, :paperclip_optimizer]
+    processors: [:thumbnail, :paperclip_optimizer],
+    path: ":public_path/system/:class/:attachment/:id_partition/:style/:filename"
 )
